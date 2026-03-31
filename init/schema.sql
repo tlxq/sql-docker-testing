0599bf10b1216800888a542abbe73260e6f00fd4
Tom Larsson <tlxqz@proton.me>
2026-03-31 20:11:00 +0200

    Adding table bilar and fix of rights.

---
+++ init/schema.sql
@@ -0,0 +1,63 @@
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19  Distrib 10.11.16-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: min_skoldatabas
+-- ------------------------------------------------------
+-- Server version	10.11.16-MariaDB-ubu2204
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bilar`
+--
+
+DROP TABLE IF EXISTS `bilar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bilar` (
+  `reg` varchar(10) DEFAULT NULL,
+  `marke` varchar(50) DEFAULT NULL,
+  `modell` varchar(50) DEFAULT NULL,
+  `arsmodell` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bilar`
+--
+
+LOCK TABLES `bilar` WRITE;
+/*!40000 ALTER TABLE `bilar` DISABLE KEYS */;
+INSERT INTO `bilar` VALUES
+('ABC123','Saab','9-5',2003),
+('DEF123','Volvo','S80',2002),
+('GHI123','Mazda','626',2001),
+('JKL123','Audi','A8',2001),
+('MNO123','BMW','323',1998),
+('PQR123','Ford','Mondeo',2001),
+('STU123','Volvo','740',1987),
+('VYX123','Volkswagen','Golf',1988),
+('ABC456','Volkswagen','Polo',2003),
+('DEF456','Toyota','Carina II',1998);
+/*!40000 ALTER TABLE `bilar` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-03-31 18:08:49
